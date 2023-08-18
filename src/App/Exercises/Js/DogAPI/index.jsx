f177cb4cd2c793a4bec7bc3669cd7b3ed10f2cc3
Iwona <iwonkabigos@gmail.com>
2023-08-18 18:23:42 +0200

    Dodanie poprawek do zadania DogAPI

--- src/App/Exercises/Js/DogAPI/index.jsx
+++ src/App/Exercises/Js/DogAPI/index.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react';
 import './styles.css';
 
+const DOG_IMAGE_PLACEHOLDER =
+  'https://meredith.nhcrafts.org/wp-content/uploads/dog-placeholder.jpg';
+
 export const DogAPI = () => {
   const [dogImageUrl, setDogImageUrl] = useState(null);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   const handleFetchDogImage = () => {
     asyncCall();
+    setIsButtonClicked(true);
     // fetch('https://dog.ceo/api/breeds/image/random');
     // .then((response) => {
     //   return response.json();
@@ -19,12 +24,22 @@ export const DogAPI = () => {
   async function asyncCall() {
     try {
       const result = await fetch('https://dog.ceo/api/breeds/image/random');
-      const jsonResult = await result.json();
-      setDogImageUrl(jsonResult.message);
+      const { status, message } = await result.json();
+      setDogImageUrl(message);
+
+      if (status !== 200) {
+        throw new Error('Cannot fetch dog image');
+      }
     } catch (error) {
       console.log(error);
     }
   }
+
+  const dogPlacehodler = () => {
+    setTimeout(() => {
+      return DOG_IMAGE_PLACEHOLDER;
+    }, 1000);
+  };
 
   return (
     <div className="dogapi-wrapper">
@@ -32,7 +47,13 @@ export const DogAPI = () => {
       <button onClick={() => handleFetchDogImage()} className="dogapi-button">
         Show me a dog
       </button>
-      <img src={dogImageUrl} alt="" className="dogapi-image" />
+      {isButtonClicked && (
+        <img
+          src={dogImageUrl ? dogImageUrl : dogPlacehodler()}
+          alt=""
+          className="dogapi-image"
+        />
+      )}
     </div>
   );
 };
