2cc03198600e62ca1aa74c03aff6dfd564ccd461
Iwona <iwonkabigos@gmail.com>
2023-09-01 08:43:36 +0200

    dodanie komentarza do firebase.js

--- src/App/Exercises/ReactExercises/FirebaseLoginForm/firebase.js
+++ src/App/Exercises/ReactExercises/FirebaseLoginForm/firebase.js
@@ -2,5 +2,5 @@ import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {};
-
+// dodanie indywidualnego klucza z firebase dodawać i usuwać kazdorazowo
 export const app = initializeApp(firebaseConfig);
