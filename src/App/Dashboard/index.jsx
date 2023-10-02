01bc4345d5826dd45e26aaab7b752bda7b1d5bf5
Iwona <iwonkabigos@gmail.com>
2023-10-02 17:58:53 +0200

    zmiana kart na Dashbordzie

--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -4,30 +4,29 @@ import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
 import { MessageIcon } from '../Components/Icons/MessageIcon';
 import { EditIcon } from '../Components/Icons/EditIcon';
 import { CodeIcon } from '../Components/Icons/CodeIcon';
-import { BlogIcon } from '../Components/Icons/BlogIcon';
-import waving from '../Images/waving.png';
 
 export const Dashboard = () => {
   const dashboardData = [
     {
       id: 1,
-      title: 'Moje CV',
-      icon: <PersonalCardIcon />,
-      description: 'podgląd cv wraz z doświadczeniem',
+      title: 'Ćwiczenia',
+      icon: <EditIcon />,
+      description: 'przegląd ćwiczeń',
       href: '',
+      // blog, formularz, FAQ, toDo list itd.
     },
     {
       id: 2,
-      title: 'Ćwiczenia',
-      icon: <EditIcon />,
-      description: 'wszystkie wykonane ćwiczenia',
+      title: 'Gry',
+      icon: <MessageIcon />,
+      description: 'chwila relaksu',
       href: '',
     },
     {
       id: 3,
-      title: 'Blog',
-      icon: <BlogIcon />,
-      description: 'wpisy blogowe o technologi front-end',
+      title: 'O kursie',
+      icon: <PersonalCardIcon />,
+      description: 'informacje o kursie',
       href: '',
     },
     {
@@ -39,22 +38,15 @@ export const Dashboard = () => {
     },
     {
       id: 5,
-      title: 'FAQ',
-      icon: <MessageIcon />,
-      description: 'odpowiedzi na najczęstsze pytania',
+      title: 'O mnie',
+      icon: <CodeIcon />,
+      description: 'kilka zdań o mnie',
       href: '',
     },
   ];
 
   return (
     <div className="dashboard">
-      <h2>
-        <img className="dashboard-wave-image" src={waving}></img>
-        Hej, tu Iwona!
-      </h2>
-      <p>
-        Poniżej znajdziesz najważniejsze informację na temat mojej działalności
-      </p>
       <div className="dashboard-cards-wrapper">
         {dashboardData.map((element) => {
           return (
---
+++ src/App/Exercises/ReactExercises/Redux/router-data.jsx
@@ -0,0 +1,10 @@
+import { Redux } from './index';
+
+export const ReduxMetaData = {
+  path: 'redux',
+  date: '04-09-2023',
+  linkLabel: 'Redux',
+  blockNo: 47,
+  element: <Redux />,
+  tags: ['redux'],
+};
