ec728fd1a264eeeccab7e4ca7a94d6a8a472bb6e
Iwona <iwonkabigos@gmail.com>
2023-10-16 18:44:05 +0200

    changed view of TechStack component

--- src/App/TechStack/index.jsx
+++ src/App/TechStack/index.jsx
@@ -5,7 +5,7 @@ import { Ts } from '../Components/Icons/Ts.jsx';
 import { Js } from '../Components/Icons/Js.jsx';
 import { React } from '../Components/Icons/React.jsx';
 import { Github } from '../Components/Icons/Github.jsx';
-import { Bitbucket } from '../Components/Icons/Bitbucket.jsx';
+import Wordpress from '../Images/tech-stack/wordpresss.svg';
 import { Jest } from '../Components/Icons/Jest.jsx';
 import { Redux } from '../Components/Icons/Redux.jsx';
 import { Firebase } from '../Components/Icons/Firebase.jsx';
@@ -14,7 +14,7 @@ import { Discord } from '../Components/Icons/Discord.jsx';
 import { Readmine } from '../Components/Icons/Readmine.jsx';
 import Vscode from '../Images/tech-stack/vscode.png';
 
-const techStackData = [
+const techStackBasics = [
   {
     src: Css,
     text: 'css',
@@ -22,10 +22,6 @@ const techStackData = [
   {
     component: <Html />,
     text: 'html',
-  },
-  {
-    component: <Ts />,
-    text: 'typescript',
   },
   {
     component: <Js />,
@@ -40,8 +36,27 @@ const techStackData = [
     text: 'github',
   },
   {
-    component: <Bitbucket />,
-    text: 'bitbucket',
+    component: <Git />,
+    text: 'git',
+  },
+  {
+    src: Vscode,
+    text: 'vscode',
+  },
+  {
+    component: <Discord />,
+    text: 'discord',
+  },
+  {
+    src: Wordpress,
+    text: 'wordpress',
+  },
+];
+
+const techStackIntro = [
+  {
+    component: <Ts />,
+    text: 'typescript',
   },
   {
     component: <Jest />,
@@ -56,18 +71,6 @@ const techStackData = [
     text: 'redux',
   },
   {
-    component: <Git />,
-    text: 'git',
-  },
-  {
-    src: Vscode,
-    text: 'vscode',
-  },
-  {
-    component: <Discord />,
-    text: 'discord',
-  },
-  {
     src: require('../Images/tech-stack/jira.png'),
     text: 'jira',
   },
@@ -76,18 +79,34 @@ const techStackData = [
     text: 'readmine',
   },
 ];
+
 export const TechStack = () => {
   return (
     <div className="tech-stack-box">
       <a href="/dashboard" className="tech-back-link">
         &#60; TECH STACK
       </a>
-      <p>
-        Poniżej znajdziesz tech stack oraz nadzędzia, jakich nauczylem się
+      <p className="tech-stack-text">
+        Poniżej znajdziesz tech stack oraz nadzędzia, jakich nauczyłam się
         podczas kursu.
       </p>
-      <div className="tech-stack-grid-div">
-        {techStackData.map((element, i) => {
+
+      <div className="tech-stack-grid ">
+        <p className="tech-stack-grid-text">Podstawy</p>
+        {techStackBasics.map((element, i) => {
+          return (
+            <div key={i}>
+              {element.component && element.component}
+              {element.src && <img src={element.src} alt={element.text} />}
+              <p>{element.text}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="tech-stack-grid ">
+        <p className="tech-stack-grid-text">Pierwsze kroki</p>
+        {techStackIntro.map((element, i) => {
           return (
             <div key={i}>
               {element.component && element.component}
